d559b3d155acd6657d59a555205c5db42c6d70ef
rolwane <rolwane@hotmail.com>
2022-08-28 09:03:23 -0300

    feat: arquivo de conexão com o banco de dados

---
+++ backend/src/models/connection.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+});
+
+module.exports = connection;
