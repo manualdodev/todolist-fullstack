38da6c66f70da6784893d332a99c14e7fa4e6515
rolwane <rolwane@hotmail.com>
2023-01-08 14:37:05 -0300

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 module.exports = app;
